d6145c125cf02b05a5f9c41b8d3ed3074baa1aa2
anatoly <tolya_ageev@bk.ru>
2020-03-08 17:17:32 +0700

    Fix cout bag

--- main.c
+++ main.c
@@ -16,35 +16,21 @@ int min_element(int* arr, int size)
     return min;
 }
 
-void print_array(int* arr, int size)
+int main()
 {
-<<<<<<< HEAD
     int arr[] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3};
 
     const int min = min_element(arr, N_ELEMENTS(arr));
 
-||||||| constructed merge base
-    int arr[] = {3, 1, 4, 1, 5, 9, 2};
-
-    const int min = min_element(arr, N_ELEMENTS(arr));
-
-=======
->>>>>>> Extract printing array to function
     printf("Array: ");
     int i;
-    for (i = 0; i < size; ++i) {
+    for (i = 0; i < N_ELEMENTS(arr); ++i)
+
+    {
         printf("%d ", arr[i]);
     }
     printf("\n");
-}
 
-int main()
-{
-    int arr[] = {3, 1, 4, 1, 5, 9, 2};
-
-    const int min = min_element(arr, N_ELEMENTS(arr));
-
-    print_array(arr, N_ELEMENTS(arr));
     printf("Min element: %d\n", min);
 
     return 0;
